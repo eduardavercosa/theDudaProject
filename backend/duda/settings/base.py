365f67af0ee867f41df7f5d995d2e91c3d18ced3
Eduarda <eduardavercosa@vinta.com.br>
2021-03-08 16:49:40 -0300

    f

--- backend/duda/settings/base.py
+++ backend/duda/settings/base.py
@@ -22,7 +22,7 @@ ADMINS = (("Admin", "eduardavercosa@vinta.com.br"),)
 
 AUTH_USER_MODEL = "users.User"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [dudapokebattle.herokuapp.com]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
