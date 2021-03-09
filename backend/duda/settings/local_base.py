df5daabbce211d2f6fcbba2b3cd65d22ade928e4
Eduarda <eduardavercosa@vinta.com.br>
2021-03-09 15:28:45 -0300

    l

--- backend/duda/settings/local_base.py
+++ backend/duda/settings/local_base.py
@@ -27,7 +27,7 @@ CELERY_TASK_ALWAYS_EAGER = True
 CELERY_TASK_EAGER_PROPAGATES = True
 
 # Email
-#INSTALLED_APPS += ("naomi",)
+INSTALLED_APPS += ("naomi",)
 EMAIL_BACKEND = "naomi.mail.backends.naomi.NaomiBackend"
 EMAIL_FILE_PATH = base_dir_join("tmp_email")
 
