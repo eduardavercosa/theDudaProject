ae68918e550c28161a3e97dc2f70cb1542aab2d2
Eduarda <eduardavercosa@vinta.com.br>
2021-03-15 15:37:50 -0300

    error not found fix

--- backend/duda/urls.py
+++ backend/duda/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url  # noqa
 from django.urls import path
 from django.contrib import admin
 from django.shortcuts import redirect
+from commom.views import homeView
 
 import django_js_reverse.views
 
@@ -9,4 +10,5 @@ import django_js_reverse.views
 urlpatterns = [
     path("admin/", admin.site.urls, name="admin"),
     path("jsreverse/", django_js_reverse.views.urls_js, name="js_reverse"),
+    path("", homeView.as_view(), name="home"),
 ]
