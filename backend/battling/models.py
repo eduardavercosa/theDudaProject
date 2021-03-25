bcbf01a3f5aa89dedc14cc0c661bb5cdfcc745d0
Eduarda <eduardavercosa@vinta.com.br>
2021-03-25 13:36:28 -0300

    fix: typos and style

--- backend/battling/models.py
+++ backend/battling/models.py
@@ -1,18 +1,10 @@
 from django.conf import settings
 from django.db import models
-from django.utils import timezone
-
-
-
-
 from urllib.parse import urljoin
-
 import requests
-#from progress.bar import ChargingBar
 
 class Gamer(models.Model):
     name = models.CharField(max_length=200)
-
 
     def publish(self):
         self.save()
@@ -31,34 +23,20 @@ class Battle(models.Model):
         POKEMONS.append((pokemon["name"],pokemon["name"]))
 
     id = models.AutoField(primary_key=True)
-    player1 = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='+', verbose_name='What is your name?',  null=True)
-    player2 = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='+', verbose_name='Choose your opponent:',  null=True)
-    pk11 = models.CharField(max_length=200, choices=POKEMONS, verbose_name='Pokemon 1:', null=True)
-    pk21 = models.CharField(max_length=200, choices=POKEMONS, verbose_name='Pokemon 2:', null=True)
-    pk31 = models.CharField(max_length=200, choices=POKEMONS, verbose_name='Pokemon 3:', null=True)
-    pk12 = models.CharField(max_length=200, choices=POKEMONS, verbose_name='Pokemon 1:', null=True)
-    pk22 = models.CharField(max_length=200, choices=POKEMONS, verbose_name='Pokemon 2:', null=True)
+    player1 = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='+', verbose_name='You are:',  null=True)
+    player2 = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='+', verbose_name='Opponent:',  null=True)
+    pk11 = models.CharField(max_length=200, verbose_name='Pokemon 1:', null=True)
+    pk21 = models.CharField(max_length=200, verbose_name='Pokemon 2:', null=True)
+    pk31 = models.CharField(max_length=200, verbose_name='Pokemon 3:', null=True)
+    pk12 = models.CharField(max_length=200, verbose_name='Pokemon 1:', null=True)
+    pk22 = models.CharField(max_length=200, verbose_name='Pokemon 2:', null=True)
     pk32 = models.CharField(max_length=200, choices=POKEMONS, verbose_name='Pokemon 3:', null=True)
     def publish(self):
         self.save()
 
 
 class Status(models.Model):
-    #id = models.AutoField(primary_key=True)
     sequence = models.IntegerField()
 
-    def publish(self):
-        self.save()
-
-class Round(models.Model):
-    #status = models.ForeignKey(settings.STATUS_MODEL, on_delete=models.CASCADE)
-    winner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='winner_gamer_round', null=True)
-    id_batalha = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='batalha_id', null=True)
-    pk11 = models.CharField(max_length=200, verbose_name='Pokemon 1:', null=True)
-    pk21 = models.CharField(max_length=200, verbose_name='Pokemon 2:', null=True)
-    pk31 = models.CharField(max_length=200, verbose_name='Pokemon 3:', null=True)
-    pk12 = models.CharField(max_length=200, verbose_name='Pokemon 1:', null=True)
-    pk22 = models.CharField(max_length=200, verbose_name='Pokemon 2:', null=True)
-    pk32 = models.CharField(max_length=200, verbose_name='Pokemon 3:', null=True)
     def publish(self):
         self.save()
--- backend/battling/views.py
+++ backend/battling/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from users.models import User
 from .models import Battle
-from .forms import BattleForm, RoundForm, RoundForm2
+from .forms import RoundForm, RoundForm2
+from urllib.parse import urljoin
+from django.conf import settings
+import requests
+
 
 
 # Create your views here.
@@ -10,27 +14,44 @@ def home(request):
     gamer = Battle.objects.all()
     return render(request, 'battling/home.html', { 'gamer' : gamer})
 
+def get_pokemon_from_api(poke_name):
+    url = urljoin(settings.POKE_API_URL, poke_name)
+    response = requests.get(url)
+    data = response.json()
+    info = {
+        "defense": data["stats"][3]["base_stat"],
+        "attack": data["stats"][4]["base_stat"],
+        "hp": data["stats"][5]["base_stat"],
+    }
+    return info
+
+
+def sumValid(pokemon):
+    sumResult = pokemon["attack"] +  pokemon["defense"] + pokemon["hp"]
+    return sumResult
+
 def battle_new(request):
     if request.method == "POST":
-        form = BattleForm(request.POST)
+        form = RoundForm(request.POST)
         if form.is_valid():
             battle = form.save(commit=False)
-            battle.save()
-            return redirect('round_new')
+            dataPK11 = get_pokemon_from_api(battle.pk11)
+            dataPK21 = get_pokemon_from_api(battle.pk21)
+            dataPK31 = get_pokemon_from_api(battle.pk31)
+            sumPK11 = sumValid(dataPK11)
+            sumPK21 = sumValid(dataPK21)
+            sumPK31 = sumValid(dataPK31)
+            sumAll = sumPK11 + sumPK21 + sumPK31
+            if (sumAll <= 600):
+                battle.save()
+                return redirect('invite')
+            if (sumAll > 600):
+                print("erro")
+                message = "ERROR: The PKNs you selected sum more than 600 points, please choose again"
+                return render(request, 'battling/battle_new.html', {'form': form, 'message': message})
     else:
-        form = BattleForm()
+        form = RoundForm()
     return render(request, 'battling/battle_new.html', {'form': form})
-
-def round_new(request):
-    # if request.method == "POST":
-    #     formRound = RoundForm(request.POST)
-    #     if formRound.is_valid():
-    #         roundBattle = formRound.save(commit=False)
-    #         roundBattle.save()
-    #         return redirect('home')
-    # else:
-    #     formRound = RoundForm()
-    return render(request, 'battling/round_new.html')
 
 def invite(request):
     return render(request, 'battling/invite.html')
@@ -39,12 +60,26 @@ def opponent(request):
     return render(request, 'battling/opponent.html')
 
 def round_new2(request):
-    battleInfo = Battle.objects.get(id=9)
+    battleInfo = Battle.objects.get(id=57)
     if request.method == "POST":
         formRound2 = RoundForm2(request.POST, instance=battleInfo)
         if formRound2.is_valid():
-            formRound2.save()
-            return redirect('home')
+            teste = formRound2.save(commit=False)
+            dataPK12 = get_pokemon_from_api(teste.pk12)
+            dataPK22 = get_pokemon_from_api(teste.pk22)
+            dataPK32 = get_pokemon_from_api(teste.pk32)
+            sumPK12 = sumValid(dataPK12)
+            sumPK22 = sumValid(dataPK22)
+            sumPK32 = sumValid(dataPK32)
+            sumAll = sumPK12 + sumPK22 + sumPK32
+            if (sumAll <= 600):
+                formRound2.save()
+                return redirect('home')
+            if (sumAll > 600):
+                message = "ERROR: The PKNs you selected sum more than 600 points, please choose again"
+                return render(request, 'battling/round_new2.html', {'formRound2': formRound2, 'battle':battleInfo, 'message': message})
     else:
         formRound2 = RoundForm2()
-    return render(request, 'battling/round_new2.html', {'formRound2': formRound2, 'battleInfo': battleInfo})
+    return render(request, 'battling/round_new2.html', {'formRound2': formRound2, 'battle':battleInfo})
+
+
