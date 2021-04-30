06d3adf7fd693498f0070ff3d1a806605d2720da
Eduarda <eduardavercosa@vinta.com.br>
2021-04-30 16:16:20 -0300

    refactor: change models

--- backend/battling/admin.py
+++ backend/battling/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Battle
+from .models import Battle, PokemonTeam, Team
 
 
 admin.site.register(Battle)
+admin.site.register(PokemonTeam)
+admin.site.register(Team)
